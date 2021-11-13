69fbb17ad086ad14b3713f5d072f5afdcd1b2f19
vuduynhiennn <vuduynhiencute@gmail.com>
2021-11-13 12:54:04 +0700

    folder select and file readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+**buoi 2 select**
+
+source database northwind: [link to download](https://drive.google.com/drive/folders/1_wHB4n3XRK1LHbwmSsRh1bLRlaCHu-ag)
+
+**cac cau lenh cua sql sever**
+
+## built in functions sql
---
+++ SELECt/select.sql
@@ -0,0 +1,12 @@
+﻿USE Northwind -- chọn để chơi với thùng chứa data nào đó	
+              -- tại 1 thời điểm chỉ chơi với 1 thùng chứa data
+
+SELECT * FROM Customers
+
+-----------------------------------------------------------------
+-- LÍ THUYẾT 
+-- 1. dbe cung cấp các câu lệnh SELECT dùng để
+--	  in ra màn hình một cái gì đó ~~ printf của C, sout() java
+--    in ra dữ liệu có trong table ( hàng, cột )
+--    dùng cho mục đích nào thì kết quả hiển thị luôn là một table
+-----------------------------------------------------------------
