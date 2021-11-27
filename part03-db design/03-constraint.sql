7dd7c4533c4b09cd56cb372942d22740a24cc276
vuduynhiennn <vuduynhiencute@gmail.com>
2021-11-27 18:39:52 +0700

    add new file

--- part03-db design/03-constraint.sql
+++ part03-db design/03-constraint.sql
@@ -1,1 +1,34 @@
-﻿
+﻿-- PRIMARY KEY PHẦN NÀY DÍNH DẾN CÁC RÀNG BUỘC
+-- CONSTRAINT CÁC QUY TẮC GÀI TRÊN DATA
+-- 1. RÀNG BUỘC PRIMARY KEY
+-- TẠM THỜI HIỂU, CHẤP NHẬN PK LÀ MỘT CỘT TƯƠNG LAI CÓ THỂ CÓ NHIỀU CỘT, MÀ GIÁ TRỊ CỦA NÓ TRÊN MỌI DÒNG CỦA CỘT CELL CỦA CỘT NÀY
+-- KHÔNG TRÙNG LẠI, MỤC ĐÍCH ĐỂ WHERE RA ĐƯỢC 1 DÒNG DUY NHẤT 
+-- VD: SỐ CHỨNG MINH NHÂN DÂN XÁC ĐỊNH DUY NHẤT MỘT CON NGƯỜI 
+-- CÁCH 1: TỰ NHẬP BẰNG TAY VÀ DB ENGINE SẼ TƯ KIỂM TRA DÙM MÌNH XEM CÓ TRÙNG KHÔNG, DB ENGINE SẼ TỰ BÁO VI PHẠM CONTRAINT HỌC RÒI 
+
+USE DBDESIGN_ONETABLE
+
+CREATE TABLE Registration (
+	SEQ INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(10),
+	LastName NVARCHAR(30),
+	Email VARCHAR(50),  -- CAM TRUNG TINH SAU
+	Phone VARCHAR(11),	-- 
+	RegDate DATETIME DEFAULT GETDATE() -- CONSTRAINT DEFAULT
+
+)
+-- ĐĂNG KÍ THỬ 
+
+INSERT INTO Registration
+VALUES (N'An', N'Nguyễn', 'an@gmail.com', '090X', DEFAULT) -- BÁO LỖI DO KHÔNG CUNG CẤP ĐỦ CỘT
+
+INSERT INTO Registration (FirstName, LastName, Email, Phone)
+VALUES (N'Linh', N'Nguyễn', 'linh@gmail.com', '090X') -- BÁO LỖI DO KHÔNG CUNG CẤP ĐỦ CỘT
+
+ 
+SELECT * FROM Registration
+
+
+
+
+
---
+++ part03-db design/04- design.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE PhoneBookV2 (
+	Nick NVARCHAR(30),
+	HomePhone char(11), -- chỉ 1 số phone thôi
+	WorkPhone2 char(11),
+	CellPhone char(11)  -- éo biết cột nào là loại nào, 1 3 3 vô hồn
+)
+
+-- MỞ RỘNG TABLE THEO CHIỀU NGANG
+SELECT p.CellPhone, P.HomePhone FROM PhoneBookV2 p
+
+INSERT INTO PhoneBookV2
+VALUES (N'Nhiên', '0353021375', '01676717928', '0973347739'), (N'An', '090231221', NULL, NULL), (N'Bình', '09X', '098X', '092')
+
+DROP TABLE PhoneBookV2
+-- THỐNG KÊ SỐ LƯỢNG, SỐ ĐT ĐANG CÓ MỖI NGƯỜI SÀI LÀ KHÔNG ĐƯUỌC
+
+SELECT COUNT(*) FROM PhoneBookV2 WHERE Nick = 'An'
